56d33de3f023ca7b13f490008b78fbad0dbcf144
longldktmm <43570753+longldktmm@users.noreply.github.com>
2024-10-19 22:58:30 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 onload = () => {
-      const titles = ("   Chúc mừng Hằng xinh gái cute siêu giỏi 20😽-10😽").split('')
+      const titles = ("   Chúc Hằng của t  20😽-10😽 xinh đẹp cute giỏi giang").split('')
       const titleElement = document.getElementsByClassName('title');
       let index = 0;
-      console.log("loaded")
       function appendTitle() {
         if (index < titles.length) {
-            console.log(index)
             document.querySelector("body > h1").innerHTML  = (document.querySelector("body > h1").innerHTML  + titles[index]);
             index++;
             setTimeout(appendTitle, 300); // 1000ms delay
